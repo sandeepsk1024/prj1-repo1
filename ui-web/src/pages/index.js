5fb7fb67a4e6dfa2e7e278650ed66103af6e5992
Sandeep Khandewale <sandeepsk1024@gmail.com>
2020-04-05 12:23:08 +0530

    temp fix for failing gatsby build

--- ui-web/src/pages/index.js
+++ ui-web/src/pages/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import connecter from '../state/connector'
-
-
 const Index = (props) => {
-    console.log(props)
     return <h1>Hello world, from index page</h1>
 }
 
-export default connecter(Index)
+export default Index
 
